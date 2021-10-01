ad3e95cf0656a1ebfb6dea82ea49ccf817cd7bdc
Koctr <Koctr1978@yandex.ru>
2021-10-01 16:23:18 +0300

    my commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+      <head>
+	      <title> Моя страничка </title>
+		  <link rel="stylesheet" href="mycss.css"
+		  <link rel="shortcut icon" href="favicon.ico">
+	</head>
+    <body>
+        <div class="mydiv1"
+            <?php
+                echo "Результат сложения 342645724 и 24574257457 равен ";
+            ?>
+        </div>
+    </body>
+</html>	
